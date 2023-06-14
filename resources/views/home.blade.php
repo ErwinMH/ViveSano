bf088562b3e5ec9bcf8ca62a4b34c2faa8ea6571
cris <crisvalmart34@gmail.com>
2023-06-13 20:40:48 -0400

    creacion de home.blade.php y home.css, inicio de nav en el home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ViveSano</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+    
+    <div class="container">
+        <nav>
+            <h1 class="logo">Vive<span>Sano</span></h1>
+            <div class="nav-links">
+                <ul>
+                    <li class=""><a href="">Home</a></li>
+                    <li class=""><a href="">Sobre nosotros</a></li>
+                    <li class=""><a href="">Especialistas</a></li>
+                    <li class=""><a href="">Recetas</a></li>
+                    <li class=""><a href="">Planes de ejercicio</a></li>
+                    <li class=""><a href="">Iniciar sesion</a></li>
+                    <li class=""><a href="">Registrarse</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="main">
+            <h2>ViveSano</h2>
+            <p>jkajshk</p>
+        </div>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
