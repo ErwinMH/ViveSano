5521c7c77df533f8f3386a80fd267fb24939613d
cris <crisvalmart34@gmail.com>
2023-06-17 16:47:12 -0400

    home: nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,44 @@
 </head>
 <body>
     
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="">ViveSano</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Planes de ejercicio</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="">kaj</a>
+                <a class="dropdown-item" href="">argo</a>
+               
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Recetas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="">Especialistas</a>
+              </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="button">Iniciar sesion</button>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="button">Registrarse</button>
+          </form>
+        </div>
+      </nav>
+
 
     <div class="banner">
         <div class="contenido">
