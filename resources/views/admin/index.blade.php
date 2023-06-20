030bd004b19cbe6d644244b449c954f5466a61eb
ErwinMH <2190017@ipisa.edu.do>
2023-06-20 19:08:54 -0400

    Configuración de vista de admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,8 +7,135 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
+
+                        <span id="card_title">
+                            {{ __('Vista de usuarios') }}
+                        </span>
+
+                         <div class="float-right">
+                            <a href="" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                              {{ __('Crear usuario') }}
+                            </a>
+                          </div>
+                    </div>
+                </div>
+                @if ($message = Session::get('success'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                @endif
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead class="thead">
+                                <tr>
+                                    <th>Id de usuario</th>
+                                    
+                                    <th>Nombre</th>
+                                    <th>Correo electrónico</th>
+                                    <th>Id Rol</th>
+                                    <th>Fecha de registro</th>
+                                    
+
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->rol }}</td>
+                                        <td>{{ $user->created_at }}</td>
+                                        
+
+                                        <td>    
+                                                <a class="btn btn-sm btn-primary"  data-toggle="modal" data-target="#myModal{{$user->id}}"><i class="fa fa-fw fa-eye"></i> {{ __('Ver usuario') }}</a>
+                                         
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            {!! $users->links() !!}
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="CreateUser" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="myModalLabel">Crear Usuario</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  @foreach($users as $usuario)
+  <!-- Modal -->
+  <div class="modal fade" id="myModal{{$usuario->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="myModalLabel">Información de Usuario</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th>Id del usuario</th>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Rol</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{{ $usuario->id }}</td>
+                    <td>{{ $usuario->name }}</td>
+                    <td>{{ $usuario->email }}</td>
+                    <td>{{ $usuario->rol }}</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+@endforeach
+
 @stop
+
+
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
