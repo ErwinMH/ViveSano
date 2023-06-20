021ab3604353c5a42eba9e7f4cb2fb548e3efb06
ErwinMH <2190017@ipisa.edu.do>
2023-06-19 20:06:51 -0400

    Vista para crear y mostrar la dieta

--- resources/views/chat/dieta.blade.php
+++ resources/views/chat/dieta.blade.php
@@ -1,6 +1,6 @@
-<h1>ChatGPT en Laravel</h1>
+<h1>Genera tu dieta</h1>
 
-<form action="{{ route('chat.sendMessage') }}" method="post">
+<form action="{{ route('dieta.sendMessage') }}" method="post">
     @csrf
     <label for="message">Mensaje:</label>
     <input type="text" name="message" id="message" />
