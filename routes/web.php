53dedade99f269867a74f7ca2cf0409caae0d217
ErwinMH <2190017@ipisa.edu.do>
2023-06-19 20:15:18 -0400

    Las rutas para acceder a las funcionalidades de la API OpenAI desde las vistas, dieta y rutina, fueron configuradas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-use App\Http\Controllers\ChatController;
+use App\Http\Controllers\DietaController;
+use App\Http\Controllers\RutinaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\registroController;
 
@@ -18,11 +19,16 @@ Route::get('/', function () {
     return view('home');
 });
 
-
 Route::get('/registrarse', [registroController::class, 'registrar'])->name('registro.index');
 
-Route::get('/chat', 'ChatController@index')->name('chat.index');
-Route::post('/chat', 'ChatController@sendMessage')->name('chat.sendMessage');
+Route::get('/dieta', 'DietaController@index')->name('dieta.index');
+Route::post('/dieta', 'DietaController@sendMessage')->name('dieta.sendMessage');
 
-Route::get('/chat', [ChatController::class, 'index'])->name('chat.index')->middleware();;
-Route::post('/chat', [ChatController::class, 'sendMessage'])->name('chat.sendMessage');
+Route::get('/rutina', 'RutinaController@index')->name('rutina.index');
+Route::post('/rutina', 'RutinaController@sendMessage')->name('rutina.sendMessage');
+
+Route::get('/dieta', [DietaController::class, 'index'])->name('dieta.index')->middleware();;
+Route::post('/dieta', [DietaController::class, 'sendMessage'])->name('dieta.sendMessage');
+
+Route::get('/rutina', [RutinaController::class, 'index'])->name('rutina.index')->middleware();;
+Route::post('/rutina', [RutinaController::class, 'sendMessage'])->name('rutina.sendMessage');
