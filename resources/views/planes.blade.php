7add79f72495147ee2db6faf4c231f4506200483
cris <crisvalmart34@gmail.com>
2023-06-20 00:26:32 -0400

    integracion de generadores (dieta y planes) en forms de sus paginas

--- resources/views/planes.blade.php
+++ resources/views/planes.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
 
+
+
+<div class="plato" id="plato">
+    <div class="container">
+        <div class="row">
+            <div class="section-title">
+                <h1>Planes de ejercicio</h1>
+                <p class="small text-uppercase">Genera rutinas de ejercicio</p>
+            </div>
+
+        </div>
+    </div>
+
+    
+<form action="{{ route('rutina.sendMessage') }}" method="post">
+    @csrf
+  <div class="form-dieta">
+
+    <input type="text" name="message" placeholder="genera una rutina de ejercicio para principiantes" id="message" class="form-control" />
+    <button type="submit" class="btn btn-warning">Enviar</button>
+  </div>
+</form>
+
+@if(isset($answer))
+    <div>
+        <strong>Respuesta:</strong> {{ $answer }}
+    </div>
+@endif
+</div>
+
 @endsection
